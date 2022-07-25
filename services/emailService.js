cf2b6af9e27920c1ce3cfa91f2bbf4d62a7807c6
Pratik Hadiya <pratikhadiya0001@gmail.com>
2022-07-25 15:29:40 +0530

    change email orientation

--- services/emailService.js
+++ services/emailService.js
@@ -1,22 +1,28 @@
 const nodemailer = require('nodemailer');
-async function sendMail ({ from, to, subject, text, html }) {
-    let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        }
-    });
 
-    let info = await  transporter.sendMail({
-        from: `Shareit <${from}>`,
-        to: to,
-        subject: subject,
-        text: text,
-        html: html
-    });
+module.exports = async ({
+  from,
+  to,
+  subject,
+  text,
+  html
+}) => {
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `Shareit <${from}>`,
+    to: to,
+    subject: subject,
+    text: text,
+    html: html
+  });
 }
-
-module.exports = sendMail;
