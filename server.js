c3d7d994090f714738e43fc82a1c58e3f3b94889
Pratik Hadiya <pratikhadiya0001@gmail.com>
2022-07-25 14:55:49 +0530

    Change frontend views

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.use(express.json());
-const connectDB = require('./config/db');
-connectDB();
+const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db')
+connectDB()
 
-// Template engine
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
-// Routes
-app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/frontend/index.html');
-});
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
+app.use(express.json())
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', require('./routes/home'))
+app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/filePreview'))
+app.use('/files/download', require('./routes/download'))
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening to port ${PORT}`)
 });
